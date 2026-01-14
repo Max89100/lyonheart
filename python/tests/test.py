9989e5f77d990f4b03acc3f36dbea248664cc0a8
maxence2 <maxence.vouette@gmail.com>
2026-01-14 22:00:16 +0100

    avancement

--- python/tests/test.py
+++ python/tests/test.py
@@ -8,8 +8,13 @@ from sympy.ntheory import isprime
 def test_is_prime(s):
     assert isprime(s) == deeplearning_library.is_prime(s)
 
+def test_tensor_communication():
+    data = [[2.0, 3.0], [4.0, 5.0]]
+    result = deeplearning_library.py_computation(data)
+    print(f"Type retourné par Rust : {type(result)}")
+    print(f"Valeurs : {result}")
 
 if __name__ == "__main__":
-       test_is_prime()
+       test_tensor_communication()
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ use pyo3::prelude::*;
 use burn::tensor::{Tensor, backend::Backend};
 use burn::backend::wgpu::WgpuDevice;
 use burn::backend::Wgpu;
+use numpy::pyo3::Python;
+use numpy::ndarray::array;
+use numpy::{ToPyArray, PyArray, PyArrayMethods};
+
 
 /// Declared rust functions
 #[pyfunction]
@@ -19,23 +23,26 @@ fn is_prime(num: u32) -> bool {
 
 // Elle ne sait pas si c'est du GPU, du CPU ou du TPU. Elle sait juste faire des maths.
 // Fonction "Générique"
-fn computation<B: Backend>(device: &B::Device) {
-    let tensor1: Tensor<B, 2> = Tensor::from_floats([[2.0, 3.0], [4.0, 5.0]], device);
+fn computation<B: Backend>(data: Vec<f32>, shape: Vec<usize>, device: &B::Device) -> Vec<f32> {
+    let tensor1: Tensor<B, 2> = Tensor::from_data(Data::from(data), device);
     let tensor2 = Tensor::ones_like(&tensor1);
     let result = tensor1 + tensor2;
-    println!("Résultat du calcul : {}", result);
+    let output_data = result.into_data();
+    return output_data.to_vec().;    
 }
 
 // Ici, on "verrouille" le type pour Python.
 #[pyfunction]
-fn py_computation() -> PyResult<()> {
+fn py_computation() -> PyResult<Vec<Vec<f32>>> {
     // On force l'utilisation de Wgpu (le GPU via WebGPU)
     type MyBackend = Wgpu; 
     let device = WgpuDevice::DefaultDevice;
     // On appelle la fonction générique en lui disant d'utiliser MyBackend
-    computation::<MyBackend>(&device);
-    Ok(())
+    let _result = computation::<MyBackend>(&device);
+    Ok(_result)
 }
+
+
 
 
 
