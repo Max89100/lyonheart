11f92dd14f30e2f6428c9d8ade6f3403eba307d5
maxence2 <maxence.vouette@gmail.com>
2026-01-17 18:10:58 +0100

    implémentation du stateful object : GpuTensor

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,6 @@ use burn::backend::Wgpu;
 use numpy::{PyArray, PyArray2, PyArrayMethods, PyReadonlyArray2, PyUntypedArrayMethods};
 
 /// Declared rust functions
-#[pyfunction]
-fn is_prime(num: u32) -> bool {
-    match num {
-        0 | 1 => false,
-        _ => {
-            let limit = (num as f32).sqrt() as u32; 
-
-            (2..=limit).any(|i| num % i == 0) == false
-        }
-    }
-}
-
-
-// Elle ne sait pas si c'est du GPU, du CPU ou du TPU. Elle sait juste faire des maths.
-// Fonction "Générique"
 fn run_burn_logic<B: Backend>(data: TensorData, device: &B::Device) -> TensorData {
     // 1. On crée le tenseur à partir du TensorData
     let tensor1: Tensor<B, 2> = Tensor::from_data(data, device);
@@ -30,6 +15,41 @@ fn run_burn_logic<B: Backend>(data: TensorData, device: &B::Device) -> TensorDat
     (tensor1 + tensor2).into_data()
 }
 
+
+//CLASSES
+#[pyclass]
+pub struct GpuTensor {
+    pub tensor: Tensor<Wgpu,2>,
+}
+
+#[pymethods]
+impl GpuTensor {
+    #[new]
+    fn new(input: PyReadonlyArray2<'_,f32>) -> Self {
+        let shape = [input.shape()[0], input.shape()[1]];
+        let data_vec = input.as_array().to_owned().into_raw_vec_and_offset().0;
+        let input_data = TensorData::new(data_vec, shape);
+        Self { 
+            tensor: Tensor::from_data(input_data, &WgpuDevice::DefaultDevice),
+        }
+    }
+    
+    fn to_numpy<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyArray2<f32>>> {
+        let tensor_data = self.tensor.clone().into_data();
+        let out_slice = tensor_data.as_slice::<f32>()
+            .map_err(|_e| PyErr::new::<pyo3::exceptions::PyTypeError, _>("Burn data conversion failed"))?;
+        let dims = self.tensor.shape().dims;
+        let shape = [dims[0], dims[1]];
+        let py_array = PyArray::from_slice(py, out_slice)
+            .reshape(shape)
+            .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("Erreur de reshape: {:?}", e)))?;
+        Ok(py_array)
+    }
+}
+
+
+
+//Python Functions
 #[pyfunction]
 fn py_computation<'py>(py: Python<'py>, input: PyReadonlyArray2<'_, f32>) -> PyResult<Bound<'py, PyArray2<f32>>> {
     type MyBackend = Wgpu;
@@ -44,7 +64,7 @@ fn py_computation<'py>(py: Python<'py>, input: PyReadonlyArray2<'_, f32>) -> PyR
     let result_data = run_burn_logic::<MyBackend>(input_data, &device);
 
     let out_slice = result_data.as_slice::<f32>()
-        .map_err(|e| PyErr::new::<pyo3::exceptions::PyTypeError, _>("Burn data conversion failed"))?;
+        .map_err(|_e| PyErr::new::<pyo3::exceptions::PyTypeError, _>("Burn data conversion failed"))?;
     // On crée l'array 1D puis on le reshape en 2D pour correspondre à la shape d'origine
     let py_array_1d = PyArray::from_slice(py, out_slice);
     let py_array_2d = py_array_1d
@@ -54,7 +74,17 @@ fn py_computation<'py>(py: Python<'py>, input: PyReadonlyArray2<'_, f32>) -> PyR
     Ok(py_array_2d)
 }
 
+#[pyfunction]
+fn is_prime(num: u32) -> bool {
+    match num {
+        0 | 1 => false,
+        _ => {
+            let limit = (num as f32).sqrt() as u32; 
 
+            (2..=limit).any(|i| num % i == 0) == false
+        }
+    }
+}
 
 
 
@@ -68,6 +98,8 @@ fn deeplearning_library(m: &Bound<'_, PyModule>) -> PyResult<()> {
 }
 
 
+
+// TESTS
 #[cfg(test)]
 mod tests {
     use super::*;
